8c731f10d937cb580ed9b762175bac479167e803
Hafijul Islam <hafijul233@gmail.com>
2022-06-17 19:31:03 +0600

    Initial commit

---
+++ resources/views/products/edit.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="d-sm-flex align-items-center justify-content-between mb-4">
+        <h1 class="h3 mb-0 text-gray-800">Edit Product</h1>
+    </div>
+    <div id="app">
+        <create-product :variants="{{ $variants }}">Loading</create-product>
+    </div>
+@endsection
