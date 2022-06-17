efa6c4ac05d737afd7bdacd8ef31f60a30b7c294
Hafijul Islam <hafijul233@gmail.com>
2022-06-17 20:17:47 +0600

    npm installed and mix run triggered

--- public/js/app.js
+++ public/js/app.js
@@ -63300,8 +63300,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /home/rifat/Programming/mediusware/interview/interview-question-sr/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /home/rifat/Programming/mediusware/interview/interview-question-sr/resources/sass/app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /media/hafijul233/HDD/WorkDrive/XAMPP/htdocs/interview-question-sr/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /media/hafijul233/HDD/WorkDrive/XAMPP/htdocs/interview-question-sr/resources/sass/app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
